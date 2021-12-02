3948a87f7211a909257b3b3ad2efbdf0a7343f20
Pavel Schoffer <xschof00@gmail.com>
2021-12-02 10:44:28 +0100

    part1

--- 2021/02/src/main.rs
+++ 2021/02/src/main.rs
@@ -4,10 +4,13 @@ mod parser;
 mod submarine;
 
 fn main() {
-    let lines = parser::parse_lines("1".to_string());
+    // let lines = parser::parse_lines("1".to_string());
+    let lines = parser::parse_lines(None);
     println!("Got lines {}, e.g. \"{}\"", lines.len(), lines[0]);
 
-    let sub = submarine::Submarine::new();
-    sub.load_instructions(lines);
-    println!("{}", sub);
+    let mut sub = submarine::Submarine::new();
+    sub.load_instructions(&lines);
+    sub.execute();
+    println!();
+    println!("RESULT is {}", sub.depth * sub.position);
 }
--- 2021/utils/submarine.rs
+++ 2021/utils/submarine.rs
@@ -1,11 +1,11 @@
 use std::fmt;
 
 pub struct Submarine {
-    ip: u32,
-    instructions: Vec<Instruction>,
+    pub ip: usize,
+    pub instructions: Vec<Instruction>,
 
-    position: i32,
-    depth: i32,
+    pub position: i32,
+    pub depth: i32,
 }
 
 impl Submarine {
@@ -19,9 +19,44 @@ impl Submarine {
         }
     }
 
-    pub fn load_instructions(&self, instruction_strings: &Vec<String>) {
+    pub fn load_instructions(&mut self, instruction_strings: &Vec<String>) {
         for instr_string in instruction_strings {
-            println!("{}", instr_string);
+            let mut parts: Vec<&str> = instr_string.split(' ').collect();
+            self.instructions.push({
+                Instruction {
+                    opcode: parts.remove(0).to_string(),
+                    arguments: parts.iter().map(|&x| x.to_string()).collect(),
+                }
+            });
+        }
+    }
+
+    pub fn do_step(&mut self) {
+        let inst = &self.instructions[self.ip];
+
+        match inst.opcode.as_str() {
+            "forward" => {
+                let steps = inst.arguments[0].parse::<i32>().unwrap();
+                self.position += steps;
+            }
+            "down" => {
+                let steps = inst.arguments[0].parse::<i32>().unwrap();
+                self.depth += steps;
+            }
+            "up" => {
+                let steps = inst.arguments[0].parse::<i32>().unwrap();
+                self.depth -= steps;
+            }
+            _ => println!("UNKOWN code"),
+        }
+
+        self.ip += 1;
+        println!("{}", self)
+    }
+
+    pub fn execute(&mut self) {
+        while self.ip < self.instructions.len() {
+            self.do_step();
         }
     }
 }
@@ -40,6 +75,13 @@ impl fmt::Display for Submarine {
 }
 
 pub struct Instruction {
-    opcode: String,
-    arguments: Vec<String>,
+    pub opcode: String,
+    pub arguments: Vec<String>,
+}
+
+impl fmt::Debug for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let args = self.arguments.join(", ");
+        write!(f, "{}({})", self.opcode, args)
+    }
 }
