6ee395ac623c196b52f8299d369a4f21aa375c2a
Pavel Schoffer <xschof00@gmail.com>
2021-12-07 06:55:06 +0100

    07_1

---
+++ 2021/07/src/main.rs
@@ -0,0 +1,86 @@
+#[path = "../../utils/parser.rs"]
+mod parser;
+use std::collections::HashMap;
+
+fn main() {
+    // let lines = parser::parse_lines("1".to_string());
+    let lines = parser::parse_lines(None);
+    println!("Got lines {}, e.g. \"{}\"", lines.len(), lines[0]);
+
+    let crabs = parser::parse_items::<usize>(&lines[0]);
+
+    let mut current_bounds = get_bounds(&crabs.keys().collect());
+    let mut current_target = get_midpoint(current_bounds);
+    let mut found_it = false;
+    while !found_it {
+        let fuel_cost = count_fuel(&crabs, current_target);
+        let mut bounds_changed = false;
+        if current_target > current_bounds.0 {
+            let left_cost = count_fuel(&crabs, current_target - 1);
+            if left_cost < fuel_cost {
+                current_bounds = (current_bounds.0, current_target - 1);
+                bounds_changed = true;
+            }
+        }
+        if current_target < current_bounds.1 {
+            let right_cost = count_fuel(&crabs, current_target + 1);
+            if right_cost < fuel_cost {
+                current_bounds = (current_target + 1, current_bounds.1);
+                bounds_changed = true;
+            }
+        }
+
+        if bounds_changed {
+            current_target = get_midpoint(current_bounds);
+        } else {
+            found_it = true;
+        }
+        println!(
+            "Crabs bounds {:?}, target {}, cost {}",
+            current_bounds, current_target, fuel_cost
+        );
+    }
+}
+
+fn get_midpoint((lowest, highest): (usize, usize)) -> usize {
+    let half_step = (highest - lowest) / 2;
+    return lowest + half_step;
+}
+
+fn get_bounds(all: &Vec<&usize>) -> (usize, usize) {
+    let mut lowest: Option<usize> = None;
+    let mut highest: Option<usize> = None;
+    for elem in all {
+        let value = **elem;
+        if lowest.is_none() {
+            lowest = Some(value);
+            highest = Some(value);
+        } else {
+            if value < lowest.unwrap() {
+                lowest = Some(value)
+            } else if value > highest.unwrap() {
+                highest = Some(value);
+            }
+        }
+    }
+
+    return (lowest.unwrap(), highest.unwrap());
+}
+
+fn count_fuel(crabs: &HashMap<usize, usize>, target: usize) -> usize {
+    let mut cost = 0;
+
+    for key in crabs.keys() {
+        if *key != target {
+            let crab_count = crabs.get(key).unwrap();
+            let distance = if *key > target {
+                *key - target
+            } else {
+                target - *key
+            };
+            cost += distance * crab_count;
+        }
+    }
+
+    return cost;
+}
--- 2021/utils/parser.rs
+++ 2021/utils/parser.rs
@@ -1,19 +1,22 @@
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::fs::File;
+use std::hash::Hash;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::str::FromStr;
 
-pub fn parse_lines<T1> ( id: T1 )-> Vec<String>
-    where
+pub fn parse_lines<T1>(id: T1) -> Vec<String>
+where
     T1: Into<Option<String>>,
 {
-
     let mut filename = "./input.txt".to_string();
     let id_intoed = id.into();
     if id_intoed.is_some() {
         filename = format!("./test{}.txt", id_intoed.unwrap());
     }
 
-    let mut final_lines : Vec<String> = Vec::new();
+    let mut final_lines: Vec<String> = Vec::new();
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
             final_lines.push(line.unwrap());
@@ -23,11 +26,34 @@ pub fn parse_lines<T1> ( id: T1 )-> Vec<String>
     return final_lines;
 }
 
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+pub fn parse_items<T>(input_str: &String) -> HashMap<T, usize>
+where
+    T: FromStr + Hash + Eq + Debug,
+    <T as FromStr>::Err: Debug,
+{
+    let mut result: HashMap<T, usize> = HashMap::new();
+
+    let ages: Vec<T> = input_str
+        .split(",")
+        .map(|item| item.parse::<T>().unwrap())
+        .collect();
+
+    for age in ages {
+        if result.contains_key(&age) {
+            let old_count = result.get(&age).unwrap().clone();
+            result.insert(age, old_count + 1);
+        } else {
+            result.insert(age, 1);
+        }
+    }
+
+    return result;
 }
