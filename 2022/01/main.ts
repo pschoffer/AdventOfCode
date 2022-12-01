681c1540791db50496913acbd446852f7efd2b2a
Pavel Schoffer <xschof00@gmail.com>
2022-12-01 09:42:15 +0000

    01_1

--- 2022/01/main.ts
+++ 2022/01/main.ts
@@ -3,7 +3,48 @@ const inputPath = path.join(__dirname, 'input.txt');
 const inputTestPath = path.join(__dirname, 'test1.txt');
 
 const run = async () => {
+    const input = parsePath(inputPath);
 
+    const calories = input.map(i => i.sum);
+    console.log(Math.max(...calories));
 }
+
+const parsePath = (inputPath: string) => {
+    const fs = require('fs');
+    const data = fs.readFileSync(inputPath, 'utf8');
+
+    const elfs: Elf[] = [];
+
+    const lines = data.split("\n");
+    let currentElf: Elf = {
+        ix: 0,
+        calories: [],
+        sum: 0,
+    }
+    for (const line of lines) {
+        if (line === "") {
+            elfs.push(currentElf);
+            currentElf = {
+                ix: elfs.length,
+                calories: [],
+                sum: 0,
+            }
+        } else {
+            currentElf.calories.push(parseInt(line));
+            currentElf.sum += parseInt(line);
+        }
+
+    }
+
+
+    return elfs;
+}
+
+interface Elf {
+    ix: number;
+    calories: number[];
+    sum: number;
+}
+
 
 run();
