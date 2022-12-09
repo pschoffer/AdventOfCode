4d5a1fd569507307559ce6a80f1af025f3c837ef
Pavel Schoffer <xschof00@gmail.com>
2022-12-09 09:26:39 +0000

    09_01

---
+++ 2022/09/main.ts
@@ -0,0 +1,67 @@
+import { getDistance2D } from "../lib/area";
+
+const { adjust2D } = require('../lib/area');
+const path = require('path');
+const inputPath = path.join(__dirname, 'input.txt');
+const inputTestPath = path.join(__dirname, 'test1.txt');
+
+const run = async () => {
+    const input = parseInput(inputPath);
+    // const input = parseInput(inputTestPath);
+
+    const visited = new Set<string>();
+    visited.add('0,0');
+    let head = [0, 0];
+    let tail = [0, 0];
+
+    for (const instruction of input) {
+        for (let step = 0; step < instruction.length; step++) {
+            head = adjust2D({ direction: instruction.direction, length: 1 }, head)
+            const tailDistance = getDistance2D(tail, head);
+            if (tailDistance.length > 1) {
+                tail = adjust2D({ direction: tailDistance.direction, length: 1 }, tail);
+                visited.add(`${tail[0]},${tail[1]}`)
+            }
+        }
+
+    }
+
+
+    console.log(visited.size)
+}
+
+const parseInput = (inputPath: string): Distance[] => {
+    const fs = require('fs');
+    const data = fs.readFileSync(inputPath, 'utf8');
+    const lines = data.split("\n");
+
+    const result: Distance[] = [];
+
+    for (const line of lines) {
+        const [directionString, distanceString] = line.split(' ');
+        let direction: Direction = 'N';
+        if (directionString === 'D') {
+            direction = 'S';
+        } else if (directionString === 'R') {
+            direction = 'E';
+        } else if (directionString === 'L') {
+            direction = 'W';
+        }
+
+        result.push({
+            direction,
+            length: parseInt(distanceString)
+        });
+    }
+
+    return result;
+}
+
+interface Distance {
+    direction: Direction;
+    length: number;
+}
+
+type Direction = 'N' | 'S' | 'E' | 'W' | 'NE' | 'NW' | 'SE' | 'SW';
+
+run();
--- 2022/lib/area.ts
+++ 2022/lib/area.ts
@@ -10,6 +10,71 @@ const isInBounds = (test: number[], edge: number[], start?: number[]): boolean =
     return true;
 }
 
-export = {
-    isInBounds
+
+interface Distance {
+    direction: Direction;
+    length: number;
+}
+
+type Direction = 'N' | 'S' | 'E' | 'W' | 'NE' | 'NW' | 'SE' | 'SW';
+
+const getDistance2D = (from: number[], to: number[]): Distance => {
+
+
+    if (from[0] === to[0] && from[1] === to[1]) {
+        return {
+            direction: 'N',
+            length: 0
+        }
+    }
+    const distance = {
+        direction: '',
+        length: 0
+    }
+
+    if (from[0] > to[0]) {
+        distance.direction += 'N';
+    } else if (from[0] < to[0]) {
+        distance.direction += 'S';
+    }
+
+    if (from[1] > to[1]) {
+        distance.direction += 'W';
+    } else if (from[1] < to[1]) {
+        distance.direction += 'E';
+    }
+
+    distance.length = Math.max(Math.abs(from[0] - to[0]), Math.abs(from[1] - to[1]));
+    return distance as Distance;
+}
+
+const adjust2D = (distance: Distance, start: number[]): number[] => {
+    let result = [...start];
+    const directions = distance.direction.split('');
+
+    for (const direction of directions) {
+        switch (direction) {
+            case 'N':
+                result[0] -= distance.length;
+                break;
+            case 'S':
+                result[0] += distance.length;
+                break;
+            case 'E':
+                result[1] += distance.length;
+                break;
+            case 'W':
+                result[1] -= distance.length;
+                break;
+        }
+    }
+
+    return result;
+}
+
+
+export {
+    isInBounds,
+    adjust2D,
+    getDistance2D,
 }
