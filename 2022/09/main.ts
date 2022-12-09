506b1153a8c68575de3f2a8fa1b12c55b415fc10
Pavel Schoffer <xschof00@gmail.com>
2022-12-09 09:42:22 +0000

    09_02

--- 2022/09/main.ts
+++ 2022/09/main.ts
@@ -4,23 +4,38 @@ const { adjust2D } = require('../lib/area');
 const path = require('path');
 const inputPath = path.join(__dirname, 'input.txt');
 const inputTestPath = path.join(__dirname, 'test1.txt');
+const inputTest2Path = path.join(__dirname, 'test2.txt');
+
+const TAIL_COUNT = 9;
 
 const run = async () => {
     const input = parseInput(inputPath);
     // const input = parseInput(inputTestPath);
+    // const input = parseInput(inputTest2Path);
 
     const visited = new Set<string>();
     visited.add('0,0');
     let head = [0, 0];
-    let tail = [0, 0];
+    let tails: number[][] = [];
+    for (let ix = 0; ix < TAIL_COUNT; ix++) {
+        tails.push([0, 0]);
+    }
 
     for (const instruction of input) {
         for (let step = 0; step < instruction.length; step++) {
             head = adjust2D({ direction: instruction.direction, length: 1 }, head)
-            const tailDistance = getDistance2D(tail, head);
-            if (tailDistance.length > 1) {
-                tail = adjust2D({ direction: tailDistance.direction, length: 1 }, tail);
-                visited.add(`${tail[0]},${tail[1]}`)
+            let leader = head;
+            for (let tailIx = 0; tailIx < tails.length; tailIx++) {
+                let tail = tails[tailIx];
+                const tailDistance = getDistance2D(tail, leader);
+                if (tailDistance.length > 1) {
+                    tail = adjust2D({ direction: tailDistance.direction, length: 1 }, tail);
+                    tails[tailIx] = tail;
+                    if (tailIx === tails.length - 1) {
+                        visited.add(`${tail[0]},${tail[1]}`)
+                    }
+                }
+                leader = tail;
             }
         }
 
