2b48a1a8619551827fcbe4a61d0b64d2c18f67b3
Pavel Schoffer <xschof00@gmail.com>
2022-12-11 07:27:30 +0000

    bigInt

--- 2022/11/main.ts
+++ 2022/11/main.ts
@@ -4,10 +4,10 @@ const path = require('path');
 const inputPath = path.join(__dirname, 'input.txt');
 const inputTestPath = path.join(__dirname, 'test1.txt');
 
-const ROUNDS = 20;
+const ROUNDS = 500;
 const run = async () => {
-    const input = parseInput(inputPath);
-    // const input = parseInput(inputTestPath);
+    // const input = parseInput(inputPath);
+    const input = parseInput(inputTestPath);
 
     const activity = simulate(input, ROUNDS);
     activity.sort((a, b) => b - a);
@@ -15,33 +15,33 @@ const run = async () => {
     console.log(activity[0] * activity[1])
 }
 
-const relief = (a: number) => Math.floor(a / 3);
 
 const simulate = (monkeys: Monkey[], rounds: number): number[] => {
     const result = new Array(monkeys.length).fill(0);
+    let prevResult = new Array(monkeys.length).fill(0);
 
     for (let i = 0; i < rounds; i++) {
         for (let monkeyIx = 0; monkeyIx < monkeys.length; monkeyIx++) {
             const monkey = monkeys[monkeyIx];
 
             while (monkey.items.length) {
-                const item = monkey.items.shift() || 0;
+                const item = monkey.items.shift() || 0n;
                 const adjusted = monkey.adjustment(item);
-                const relieved = relief(adjusted);
-                const testResult = monkey.test(relieved);
+                const testResult = monkey.test(adjusted);
 
                 const newMonkeyIx = testResult ? monkey.resultTrue : monkey.resultFalse;
-                monkeys[newMonkeyIx].items.push(relieved);
+                monkeys[newMonkeyIx].items.push(adjusted);
 
                 result[monkeyIx]++;
             }
         }
 
-        // console.log("Round ", i + 1, 'done');
-        // for (const monkey of monkeys) {
-        //     console.log(monkey.id, monkey.items)
-        // }
-        // console.log()
+        if ((i + 1) % 100 === 0) {
+            const diff = result.map((r, ix) => r - prevResult[ix]);
+            prevResult = [...result];
+            console.log("Round ", i + 1, 'done', result, diff);
+            console.log()
+        }
     }
 
     return result;
@@ -60,17 +60,17 @@ const parseInput = (inputPath: string): Monkey[] => {
 const parseMonkey = (lines: string[]): Monkey => {
 
     const id = Number((lines[0].match(/Monkey (\d+):/) || [])[1]);
-    const items = lines[1].split(': ')[1].split(',').map(Number);
+    const items = lines[1].split(': ')[1].split(',').map(BigInt);
     const operationMatch = lines[2].split(': ')[1].match(/new = old ([*+]) (.+)/) || [];
-    const adjustment = (a: number) => {
-        const b = operationMatch[2] === 'old' ? a : Number(operationMatch[2]);
+    const adjustment = (a: bigint) => {
+        const b = operationMatch[2] === 'old' ? a : BigInt(operationMatch[2]);
         if (operationMatch[1] === '*') {
             return a * b;
         }
         return a + b;
     }
     const divisibilityMatch = lines[3].match(/divisible by (\d+)/) || [];
-    const test = (a: number) => a % Number(divisibilityMatch[1]) === 0;
+    const test = (a: bigint) => a % BigInt(divisibilityMatch[1]) === 0n;
     const resultTrue = Number(lines[4].split(' ')[9]);
     const resultFalse = Number(lines[5].split(' ')[9]);
 
@@ -86,9 +86,9 @@ const parseMonkey = (lines: string[]): Monkey => {
 
 interface Monkey {
     id: number;
-    items: number[];
-    adjustment: (a: number) => number;
-    test: (a: number) => boolean;
+    items: bigint[];
+    adjustment: (a: bigint) => bigint;
+    test: (a: bigint) => boolean;
     resultTrue: number;
     resultFalse: number;
 }
