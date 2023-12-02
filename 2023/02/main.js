38a984fbddb84310ab1451b8263874fc0b989c09
Pavel Schoffer <schoffer.pavel@gmail.com>
2023-12-02 21:49:47 +0100

    02_01

---
+++ 2023/02/main.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const inputPath = path.join(__dirname, 'input.txt');
+const inputTestPath = path.join(__dirname, 'test1.txt');
+const { sum } = require(__dirname + '/../lib/math')
+
+const run = async () => {
+    const input = parsePath(inputPath);
+
+    const possibleGames = input.filter(isGamePossible).map(game => game.gameId);
+
+    console.log(sum(possibleGames));
+
+}
+
+const COLORS = ['red', 'green', 'blue'];
+const MAX_CUBES = {
+    'red': 12,
+    'green': 13,
+    'blue': 14,
+}
+const isGamePossible = (game) => {
+    for (const round of game.rounds) {
+        for (const color of COLORS) {
+            if ((round.cubes[color] || 0) > MAX_CUBES[color]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+const parsePath = (inputPath) => {
+
+    const fs = require('fs');
+    const data = fs.readFileSync(inputPath, 'utf8');
+
+    const lines = data.split('\n');
+
+    const games = [];
+    for (const line of lines) {
+        // Game 1: 10 red, 7 green, 3 blue; 5 blue, 3 red, 10 green; 4 blue, 14 green, 7 red; 1 red, 11 green; 6 blue, 17 green, 15 red; 18 green, 7 red, 5 blue
+        const [gameIdString, rest] = line.split(':');
+
+        const gameId = Number(gameIdString.replace('Game ', ''));
+
+        const roundStrings = rest.trim().split(';');
+
+        const rounds = [];
+        for (const round of roundStrings) {
+            const items = round.trim().split(', ');
+
+            const cubes = {};
+
+            for (const item of items) {
+                const [countString, color] = item.split(' ');
+
+                const count = Number(countString);
+                cubes[color] = count;
+            }
+
+
+            rounds.push({
+                cubes
+            })
+
+        }
+
+        games.push({
+            gameId,
+            rounds
+        })
+
+    }
+
+    return games;
+}
+
+
+run();
---
+++ 2023/lib/math.js
@@ -0,0 +1,15 @@
+const sum = (arr) => arr.reduce((a, b) => a + b, 0);
+
+
+function group(arr, size) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
+}
+
+module.exports = {
+    sum,
+    group
+}
