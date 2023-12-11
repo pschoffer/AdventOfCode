3b89f6dbb5c1a57545d4f9ba7ea15c1c55bb13d1
Pavel Schoffer <schoffer.pavel@gmail.com>
2023-12-11 21:23:08 +0100

    before cach

--- 2023/08/main.js
+++ 2023/08/main.js
@@ -6,27 +6,59 @@ const { sum, mul } = require(__dirname + '/../lib/math')
 const run = async () => {
     const input = parsePath(inputPath);
 
-    const steps = findZZZ(input, 0, 'AAA', 0);
+
+
+    const steps = findLength(input)
+
     console.log(steps);
 
 }
 
-const findZZZ = (input) => {
-    let ip = 0;
-    let current = 'AAA';
-    let length = 0;
+const findLength = (input) => {
+    const starts = Object.keys(input.nodes).filter(node => node.endsWith('A'));
+    const ends = Object.keys(input.nodes).filter(node => node.endsWith('Z'));
 
-    while (current !== 'ZZZ') {
-        console.log(`${length}: ${current}`);
-        const childIx = input.instructions[ip] === 'L' ? 0 : 1;
-        ip = ip === input.instructions.length - 1 ? 0 : ip + 1;
-        current = input.nodes[current][childIx];
+    const statuses = starts.map(start => ({
+        start,
+        current: start,
+        ip: 0,
+        length: 0,
+        cache: {}
+    }));
 
-        length++;
+    let allDone = false;
+
+    let counter = 0;
+    while (!allDone) {
+        counter++;
+        const shortestStatus = statuses[0];
+        if (counter % 1000 === 0) {
+            console.log(`${shortestStatus.start} ${shortestStatus.length}: ${shortestStatus.current} ${shortestStatus.ip}`);
+        }
+
+        findEnd(input, shortestStatus, ends);
+
+        statuses.sort((a, b) => a.length - b.length);
+
+        const lengths = statuses.map(status => status.length);
+        allDone = lengths.every(length => length === lengths[0]);
     }
 
+    return statuses[0].length;
+}
 
-    return length;
+const findEnd = (input, status, ends) => {
+
+    do {
+        const childIx = input.instructions[status.ip] === 'L' ? 0 : 1;
+        status.ip = status.ip === input.instructions.length - 1 ? 0 : status.ip + 1;
+        status.current = input.nodes[status.current][childIx];
+
+        status.length++;
+    } while (!ends.includes(status.current))
+
+
+    return status.length;
 
 }
 
