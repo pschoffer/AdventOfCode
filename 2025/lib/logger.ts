02ace497dbadf436cae60ae7f3ad712a2d473585
Pavel Schoffer <schoffer.pavel@gmail.com>
2025-12-11 21:08:28 +0100

    logger

---
+++ 2025/lib/logger.ts
@@ -0,0 +1,10 @@
+export const timeLog = (startTime: number, text: string) => {
+    const elapsed = Date.now() - startTime;
+    const seconds = Math.floor(elapsed / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    const remainingSeconds = seconds % 60;
+    const timeStr = `${hours.toString().padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+    console.log(`[${timeStr} - ${new Date().toTimeString()}] ${text}`)
+}
