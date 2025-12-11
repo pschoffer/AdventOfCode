20481b308f6511d224fb35dd61bca9b2db360e65
Pavel Schoffer <schoffer.pavel@gmail.com>
2025-12-11 08:37:49 +0100

    getting there

--- 2025/10/main.ts
+++ 2025/10/main.ts
@@ -20,47 +20,7 @@ const run = async () => {
             mode: 'incr',
         }
 
-        while (machine.targetKey !== JSON.stringify(state.joltage)) {
-            if (state.mode === 'incr') {
-                const btn = machine.buttons[state.currentBtnIx]!
-                const canPress = Math.min(...btn.map(joltIx => machine.targetJoltage[joltIx]! - state.joltage[joltIx]!));
-                state.btnPressed[state.currentBtnIx]! += canPress
-                if (state.currentBtnIx < machine.buttons.length - 1) {
-                    state.currentBtnIx++;
-                } else {
-                    state.mode = 'dec';
-                }
-            } else {
-
-                if (state.btnPressed[state.currentBtnIx]! > 0) {
-                    let moveToNextOne = state.currentBtnIx === machine.buttons.length - 1;
-                    if (!moveToNextOne) {
-                        const neededIxs: number[] = []
-                        for (let jIx = 0; jIx < machine.targetJoltage.length; jIx++) {
-                            if (machine.targetJoltage[jIx]! - state.joltage[jIx]!) {
-                                neededIxs.push(jIx);
-                            }
-                        }
-                        const remainingBtnIxs = [...machine.buttons]
-                            .slice(state.currentBtnIx + 1)
-                            .reduce((prev, curr) => prev.concat(curr), [])
-                        moveToNextOne = neededIxs.some(neededIx => !remainingBtnIxs.includes(neededIx))
-                    }
-
-
-                    if (moveToNextOne) {
-                        state.btnPressed[state.currentBtnIx] = 0;
-                        state.currentBtnIx--;
-                    } else {
-                        state.btnPressed[state.currentBtnIx]!--
-                        state.currentBtnIx++;
-                        state.mode = 'incr'
-                    }
-                } else {
-                    state.currentBtnIx--;
-                }
-            }
-
+        const updateJoltage = () => {
             const newJoltage = machine.targetJoltage.map(j => 0);
             for (let btnIx = 0; btnIx < state.btnPressed.length; btnIx++) {
                 const count = state.btnPressed[btnIx];
@@ -71,6 +31,85 @@ const run = async () => {
                 }
             }
             state.joltage = newJoltage;
+        }
+
+        const canBeFilledWithRemaining = () => {
+            const remainingBtns = [...machine.buttons].splice(state.currentBtnIx + 1);
+            const neededJIxs: number[] = [];
+            const filledJIxs: number[] = [];
+            for (let jIx = 0; jIx < machine.targetJoltage.length; jIx++) {
+                if (machine.targetJoltage[jIx]! - state.joltage[jIx]!) {
+                    neededJIxs.push(jIx);
+                } else {
+                    filledJIxs.push(jIx);
+                }
+            }
+            const filteredBtns = remainingBtns
+                .filter(btn => btn.every(jIx => !filledJIxs.includes(jIx)))
+            const usableJIxs = [... new Set(filteredBtns
+                .reduce((prev, curr) => prev.concat(curr), []))];
+            return neededJIxs.every(jIx => usableJIxs.includes(jIx));
+        }
+
+        while (machine.targetKey !== JSON.stringify(state.joltage)) {
+            if (state.mode === 'incr') {
+                const btn = machine.buttons[state.currentBtnIx]!
+                const canPress = Math.min(...btn.map(joltIx => machine.targetJoltage[joltIx]! - state.joltage[joltIx]!));
+                state.btnPressed[state.currentBtnIx]! += canPress
+                updateJoltage();
+
+
+                if (state.currentBtnIx < machine.buttons.length - 1) {
+                    const canBeFilled = canBeFilledWithRemaining();
+                    if (canBeFilled) {
+                        state.currentBtnIx++;
+                    } else {
+                        state.mode = 'dec';
+                    }
+                } else {
+                    state.mode = 'dec';
+                }
+            } else {
+
+                if (state.btnPressed[state.currentBtnIx]! > 0) {
+                    let lastItem = state.currentBtnIx === machine.buttons.length - 1;
+                    // if (!moveToNextOne) {
+                    //     const neededIxs: number[] = []
+                    //     for (let jIx = 0; jIx < machine.targetJoltage.length; jIx++) {
+                    //         if (machine.targetJoltage[jIx]! - state.joltage[jIx]!) {
+                    //             neededIxs.push(jIx);
+                    //         }
+                    //     }
+                    //     const remainingBtnIxs = [...machine.buttons]
+                    //         .slice(state.currentBtnIx + 1)
+                    //         .reduce((prev, curr) => prev.concat(curr), [])
+                    //     moveToNextOne = neededIxs.some(neededIx => !remainingBtnIxs.includes(neededIx))
+                    // }
+
+
+                    if (lastItem) {
+                        state.btnPressed[state.currentBtnIx] = 0;
+                        state.currentBtnIx--;
+                        updateJoltage();
+                    } else {
+                        state.btnPressed[state.currentBtnIx]!--
+                        updateJoltage();
+
+
+                        if (canBeFilledWithRemaining()) {
+                            state.currentBtnIx++;
+                            state.mode = 'incr'
+                        } else {
+                            state.btnPressed[state.currentBtnIx] = 0;
+                            state.currentBtnIx--;
+                            updateJoltage();
+                        }
+                    }
+                } else {
+                    state.currentBtnIx--;
+                }
+            }
+
         }
 
         const pressed = state.btnPressed.reduce((prev, curr) => prev + curr);
