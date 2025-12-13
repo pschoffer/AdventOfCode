ad33b47c7fbb4a115ab6e2f94b3b8de0c71f01f6
Pavel Schoffer <schoffer.pavel@gmail.com>
2025-12-13 11:36:40 +0100

    brute force 2

---
+++ 2025/12/main.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { explode, parseArea } from '../lib/area.js';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const inputPath = path.join(__dirname, 'input.txt');
+const inputTestPath = path.join(__dirname, 'test1.txt');
+
+const run = async () => {
+    const input = parseInput(inputTestPath);
+    console.log(JSON.stringify(input, null, 2))
+}
+
+const parseInput = (inputPath: string) => {
+    const data = fs.readFileSync(inputPath, 'utf8');
+    const lines: string[] = data.split("\n");
+
+    const result = [];
+
+    for (const line of lines) {
+        result.push(line);
+    }
+
+    return result;
+}
+
+run();
